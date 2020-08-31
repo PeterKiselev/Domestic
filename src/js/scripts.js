5e2db590b880713f9a53e2f3d5a9fce150f16563
Kiselev Peter <kiselevpetr3@gmail.com>
2020-08-31 23:25:27 +0300

    Domestic

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -19,17 +19,6 @@ function menuLinks(){
 window.addEventListener('resize', menuLinks);
 menuLinks();
 
-//to top window
-arrowTop.onclick = function() {
-    window.scrollTo(pageXOffset, 0);
-    // после scrollTo возникнет событие "scroll", так что стрелка автоматически скроется
-};
-
-window.addEventListener('scroll', function() {
-    arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
-});
-
-
 // slider
 var swiper = new Swiper('.swiper-container', {
     slidesPerGroup: 1,
